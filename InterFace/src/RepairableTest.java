7b99f45a78e0667a8ea0565dd82dae5399d93c91
DONGHOON LEE <mychekup@gmail.com>
2015-01-03 12:01:20 +0900

    인터페이스의 장점

--- InterFace/src/RepairableTest.java
+++ InterFace/src/RepairableTest.java
@@ -5,7 +5,7 @@ public class RepairableTest {
 	public static void main(String[] args) {
 		Tank tank = new Tank();
 		Dropship dropship = new Dropship();
-		Marine marine = new Marine();
+//		Marine marine = new Marine(); 
 		SCV scv = new SCV();
 		
 		scv.repair(tank);
